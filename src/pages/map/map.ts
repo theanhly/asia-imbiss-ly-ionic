3bc6fda19ad7db0aba3f918295019bdc608d3cd0
The-Anh <theanhly91@gmail.com>
2017-09-02 02:19:40 +0200

    add href to marker description in map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -31,7 +31,7 @@ export class MapPage {
     });
 
     let infoWindow = new google.maps.InfoWindow({
-      content: `<h5>Asia Imbiss Ly</h5><p><b>Standort</b></p><p>Strasse: Marschallstr. 1a</p><p>Ort: 13585 Berlin</p><p>Tel.: 030/3751421</p><p><b>Lieferzeiten</b></p><p>So.-Fr. von 12<sup>00</sup> bis 22<sup>00</sup> Uhr</p><p>Sa. RUHETAG</p>`
+      content: `<h5>Asia Imbiss Ly</h5><p><b>Standort</b></p><p>Strasse: Marschallstr. 1a</p><p>Ort: 13585 Berlin</p><p>Tel.: 030/3751421</p><p><b>Lieferzeiten</b></p><p>So.-Fr. von 12<sup>00</sup> bis 22<sup>00</sup> Uhr</p><p>Sa. RUHETAG</p><p><a target="_blank" style="text-decoration: none" href="https://www.google.com/maps/place/Asia+Imbiss-Ly/@52.550007,13.201773,18z/data=!4m5!3m4!1s0x0:0xc8c88f17ec32802f!8m2!3d52.5507547!4d13.1996328?hl=de-DE">In Google Maps ansehen</a></p>`
     });
 
     let marker = new google.maps.Marker({
